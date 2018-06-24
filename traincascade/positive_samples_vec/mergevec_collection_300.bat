35f3e3d3e670d6bed2d620848532f352108837e9
Mr.Bluyee <mr.bluyee@hotmail.com>
2018-06-24 14:00:58 +0800

    Initial commit

---
+++ traincascade/positive_samples_vec/mergevec_collection_300.bat
@@ -0,0 +1,3 @@
+mergevec.exe vec_collection_300.txt positive_samples_collection_300.vec
+
+pause
---
+++ traincascade/trancascade_samples_50.bat
@@ -0,0 +1,3 @@
+opencv_traincascade.exe -data data -vec positive_samples_vec\positive_sample_50.vec -bg bg.txt -numPos 200 -numNeg 5000 -w 50 -h 50
+
+pause
